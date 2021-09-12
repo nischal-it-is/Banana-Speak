feb02f2d6f13283e8930c8c70074267988b00f7f
Nischal Singh <singh.nischal.99@gmail.com>
2021-09-12 20:52:07 +0530

    Project completed
    Api call added and comments also added for explanation!

--- app.js
+++ app.js
@@ -5,10 +5,32 @@ var inputText=document.querySelector('#input');
 //     console.log(inputText.value);
 // })
 var outputText=document.querySelector("#output");
+//api
+var server = "https://api.funtranslations.com/translate/minion.json"
+
+//to construct url to send to api call
+function makeURL(input) {
+    return server + "?" + "text=" + input
+}
+//handling error if api call fails
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
 function clickHandler(){
-    console.log('Clicked');
-    console.log(inputText.value);
-    outputText.textContent="hey output  text is working";
+    // console.log('Clicked');
+    // console.log(inputText.value);
+    // outputText.textContent="hey output  text is working";
+    var input=inputText.value;
+    fetch(makeURL(input))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText; // output
+       })
+    .catch(errorHandler)
+
+    //steps to convert to api call 1.make url 2.convert the response to json 3. modify the json 4.handle error
 };
 buttonTranslate.addEventListener('click',clickHandler);
