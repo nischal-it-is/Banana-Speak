290b3075f1f637d61f1c3b92f5719482822a428c
Nischal Singh <singh.nischal.99@gmail.com>
2021-09-12 20:16:10 +0530

    Input Output handling done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var buttonTranslate=document.querySelector("#translate");
 var inputText=document.querySelector('#input');
+// inputText.addEventListener('onChange',function change()
+// {
+//     console.log(inputText.value);
+// })
+var outputText=document.querySelector("#output");
 
 function clickHandler(){
     console.log('Clicked');
     console.log(inputText.value);
+    outputText.textContent="hey output  text is working";
 };
 buttonTranslate.addEventListener('click',clickHandler);
